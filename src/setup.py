0b4acb6adbd03c796d75ed1ac8d2e0b04480f833
Vagrant <vagrant@vagrant-debian-squeeze-64.vagrantup.com>
2013-01-15 12:19:13 +0100

    all modules and dependencies within filesystem

--- src/setup.py
+++ src/setup.py
@@ -7,16 +7,16 @@ setup(
     license='MIT license',
     long_description=open('README.txt').read(),
     include_package_data=True,
-    #install_requires=[
-#	"bottle == 0.11.4",
-#	"MySQL-python == 1.2.3",
-#	"gevent == 0.13.8",
- #   ],
     install_requires=[
-	"bottle",
-	"MYSQL-python",
-	"gevent",
-	"sqlparse"
-    ]
+	"bottle == 0.11.4",
+	"MySQL-python == 1.2.3",
+	"gevent == 0.13.8",
+    ],
+#    install_requires=[
+#	"bottle",
+#	"MYSQL-python",
+#	"gevent",
+#	"sqlparse"
+ #   ]
 )
 
